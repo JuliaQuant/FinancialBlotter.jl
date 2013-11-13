0a37f49b9a29a193612c3977cfc8a88b9a0f4cf3
milktrader <milktrader@gmail.com>
2013-11-13 13:27:49 -0500

    plot has canonical julia colors including shadows

--- src/plots.jl
+++ src/plots.jl
@@ -10,26 +10,27 @@ function plotequity(df::DataFrame, col::String, fm::Int,
   x = [0:nrow(d)-1]
   y = equity(d[col]) # take the column passed into function
   y[1] = 1.0    # replace NA with starting equity
-#  push!(y, 1.0) # get back to even for plotting
 
   # useful plot variables
   neg  = y .< 1. 
   y1   = ones(length(y))
-  wash = Float64[]
+  onlybad = Float64[]
   for i in 1:length(neg)
     if neg[i] == false
-      push!(wash, 1.0)
+      push!(onlybad, 1.0)
     else
       j=i
-      push!(wash, y[j])
+      push!(onlybad, y[j])
     end
   end
 
-  # build plot
+  # build plot with canonical Julia colors
   h = FramedPlot(title="Equity Curve")
   add(h, FillBetween(x,y1, x,y, color=0x6bab5b))
-  add(h, FillBetween(x,wash, x,y1, color=0xd66661))
-  add(h, Curve(x,y))
+  add(h, FillBetween(x,onlybad, x,y1, color=0xd66661))
+  add(h, Curve(x,y, color=0x3b972e))
+  add(h, Curve(x,onlybad, color=0xc93d39))
+  add(h, Curve(x,y1, color=0xffffff))
   return h
 end
 
