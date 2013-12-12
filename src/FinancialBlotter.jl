a8d6bf45089fd816fcf383220637d644f2c71ac8
milktrader <milktrader@gmail.com>
2013-12-12 09:55:20 -0500

    more progress on name change and merge

---
+++ src/FinancialBlotter.jl
@@ -0,0 +1,39 @@
+using DataFrames, DataArrays, Datetime, TimeSeries, Winston
+
+module FinancialBlotter 
+
+using DataFrames, DataArrays, Datetime, TimeSeries, Winston
+
+export Stock,
+       Blotter
+       yahoo,
+       yahoo_stock,
+       fred,
+# future month abbreviations
+       F,
+       G,
+       H,
+       J,
+       K,
+       M,
+       N,
+       Q,
+       U,
+       V,
+       X,
+       Z,
+# testsuite macro
+       @testfinancialblotter
+
+include("io.jl")
+include("stocks.jl")
+include("futures.jl")
+include("options.jl")
+include("currencies.jl")
+include("notes.jl")
+include("blotter.jl")
+include("ledger.jl")
+include("plots.jl")
+include("testfinancialblotter.jl")
+
+end 
