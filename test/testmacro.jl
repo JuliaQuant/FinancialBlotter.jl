fa1472f982be1863a95e14696dabd0c4c997c399
milktrader <milktrader@gmail.com>
2013-12-13 12:42:09 -0500

    fixed other typo in testmacro

--- test/testmacro.jl
+++ test/testmacro.jl
@@ -1,14 +1,4 @@
 macro financialblotter()
   println("")
-  reload(Pkg.dir("FinancialInstrument/test/runtests.jl"))
-end
-
-########### time trial wrapper #####################################
-
-function timetrial(f::Function, v::Any, n::Int)
-  p = Float64[]
-  for i in 1:n+1
-    push!(p, @elapsed f(v))
-  end
-  mean(p[2:end]) # toss out the first execution from the average
+  reload(Pkg.dir("FinancialBlotter/test/runtests.jl"))
 end
